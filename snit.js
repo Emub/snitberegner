9fbc4f0e3dc7c8a276f881f1e72ec060feca9b0d
Emil <sodavansjuice@gmail.com>
2022-02-09 12:23:30 +0100

    Version 1.1
    Scriptet tager nu højde for eventuelle ekspandérbare karakterer.
    Scriptet tager nu højde for eventuelle loading-tider.

--- snit.js
+++ snit.js
@@ -1,36 +1,30 @@
-// Definér variabler
-var timer = 100,
-    grades = [],
-    snit = 0,
-    hasrun = false;
-
-// Tilføj CSS til
-
 // main-funktion
 init = function(){
 
-    // Udregn ikke snit to gange
-    if(hasrun){
-        return false;
-    }
-
-    hasrun = true;
-
+    // Definér variabler
+     var grades = [],
+         snit = 0;
+    
     // Definér tabel som variabel
     table = document.querySelector("table.Data tbody");
+
+    // Fjern snitlinjen hvis denne eksisterer, da funktionen nu vil genberegne denne
+    if(document.querySelector("tr.snit")) document.querySelector("tr.snit").remove();
 
     // Kør funktion for hver linje i tabellen
     Array.from(table.children).forEach(function(element, i){
 
-        // Check om resultatet er et tal (Alternativ er bestået/ikke bestået)
-        if(!isNaN(parseInt(element.children[4].innerHTML))){
-            grades.push({
-                ECTS: parseInt(element.children[6].innerHTML),
-                res: parseInt(element.children[4].innerHTML)
-            });
-        } else {
+        if(element.getAttribute("class").indexOf("childRow") == -1){
 
-            // Eventuelt lav en bestået%
+            // Check om resultatet er et tal (Alternativ er bestået/ikke bestået)
+            if(!isNaN(parseInt(element.children[element.children.length - 3].innerHTML))){
+                grades.push({
+                    ECTS: parseInt(element.children[element.children.length - 1].innerHTML),
+                    res: parseInt(element.children[element.children.length - 3].innerHTML)
+                });
+            } else {
+                // Eventuelt lav en bestået%
+            }
         }
     });
 
@@ -54,20 +48,34 @@ init = function(){
 
     // Mimér DOM-strukturen fra tabellen
     snitLinje.setAttribute("class", "DataSelect snit");
-    snitLinje.innerHTML = '<td class="DataValue" valign="top"></td><td class="DataValue" valign="top">ECTS-vægtet gennemsnit</td><td class="DataValue" align="center" valign="top"></td><td class="DataValue" align="center" valign="top"></td><td class="DataValue" valign="top" align="center">' + snit + '</td><td class="DataValue" valign="top" align="center"></td><td class="DataValue" align="right" valign="top"></td>'
+
+    // Test antal felter i tabel, da denne varierer
+    if(table.children[0].children.length == 8){
+        snitLinje.innerHTML = '<td class="DataValue" valign="top"></td><td class="DataValue" valign="top"></td><td class="DataValue" valign="top">ECTS-vægtet gennemsnit</td><td class="DataValue" align="center" valign="top"></td><td class="DataValue" align="center" valign="top"></td><td class="DataValue" valign="top" align="center">' + snit + '</td><td class="DataValue" valign="top" align="center"></td><td class="DataValue" align="right" valign="top"></td>'
+
+    }
+
+    else {
+        snitLinje.innerHTML = '<td class="DataValue" valign="top"></td><td class="DataValue" valign="top">ECTS-vægtet gennemsnit</td><td class="DataValue" align="center" valign="top"></td><td class="DataValue" align="center" valign="top"></td><td class="DataValue" valign="top" align="center">' + snit + '</td><td class="DataValue" valign="top" align="center"></td><td class="DataValue" align="right" valign="top"></td>'
+    }
 
     // Tilføj linjen til tabellen
     table.appendChild(snitLinje);
 }
 
-// Kør ikke funktionen før karakterer er loaded
+
 checkinit = function(){
-    if(document.querySelector("table.Data").children.length > 0){
-        init();
+    if(document.querySelector("table.Data")){
+
+        // Table eksisterer, kør nu funktionen 50 gange over de næste 5 sekunder, for at imødekomme eventuelle load-tider
+        var initLoop = setInterval(init, 100);
+        setTimeout(() => {
+            clearInterval(initLoop);
+        }, 5000);
     }
 
     else {
-        window.setTimeout(checkinit, timer);
+        window.setTimeout(checkinit, 10);
     }
 }
 
